f29e34bb4e99618c78bb450323972de1a45e5a62
WoutDepeuter <123627621+WoutDepeuter@users.noreply.github.com>
2024-11-29 14:01:00 +0100

    aanmaken file

---
+++ src/main/java/org/ehb/wout/torsync/TorsyncApplication.java
@@ -0,0 +1,13 @@
+package org.ehb.wout.torsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ehb/wout/torsync/TorsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ehb.wout.torsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
